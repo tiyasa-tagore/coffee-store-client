2da2b4b11724218f6362374eada2c27c4d7fe2ea
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-19 19:13:55 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>add cofee</h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>update coffe</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
